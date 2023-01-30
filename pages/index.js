4edadf222100f4e8f01288b1e06f87e02f649b4c
Captain Price <murathoncu@gmail.com>
2023-01-31 02:46:54 +0300

    project started

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Instagram | Captain Price</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="flex h-screen justify-center items-center">
+        Project Started
+      </h1>
+    </>
+  );
+}
