e168e8563bb82bcaea4b66d369fb55084cdf7954
Captain Price <murathoncu@gmail.com>
2023-02-01 15:19:52 +0300

    update active user

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,17 +1,24 @@
+import { signOut, useSession } from "next-auth/react";
+
 const MiniProfile = () => {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
-        src="https://pbs.twimg.com/profile_images/1589564512599932929/JuGsRJNz_400x400.jpg"
+        src={session?.user?.image}
         className="w-16 h-16 rounded-full p-[2px]"
       />
 
       <div className="flex-1 mx-4">
-        <h2 className="font-bold">murathoncu</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
 
-      <button className="text-blue-400 text-sm font-semibold">Sign out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+        Sign out
+      </button>
     </div>
   );
 };
--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,9 +1,11 @@
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { createRandomUser, USERS } from "../utils/faker";
 import Story from "./Story";
 
 const Stories = () => {
   const [randomUsers, setRandomUsers] = useState([]);
+  const { data: session } = useSession();
   useEffect(() => {
     Array.from({ length: 20 }).forEach(() => {
       USERS.push(createRandomUser());
@@ -14,6 +16,9 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {randomUsers.map((profile) => (
         <Story
           key={profile.userId}
