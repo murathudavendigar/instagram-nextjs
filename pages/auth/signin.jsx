ef8d574a4ce1b725720179c5a87d2a2125aa4279
Captain Price <murathoncu@gmail.com>
2023-02-01 15:18:46 +0300

    add router to main page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -6,13 +6,27 @@ const signInPage = ({ providers }) => {
   return (
     <>
       <Header />
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://marka-logo.com/wp-content/uploads/2020/04/Instagram-Logo.png"
+          alt=""
+        />
+        <p className="font-medium italic">
+          This is not a Real app, it is built for education purposes only
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
