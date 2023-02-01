8127158ac21b32b03bc9d698c0258552485f5662
Captain Price <murathoncu@gmail.com>
2023-02-01 20:48:42 +0300

    done comment

--- components/Post.jsx
+++ components/Post.jsx
@@ -6,8 +6,52 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
+
 import { HeartIcon as HeartFillIcon } from "@heroicons/react/solid";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { db } from "../firebase/firebase";
+import Moment from "react-moment";
+
 const Post = ({ id, username, userImg, img, caption }) => {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db]
+  );
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
@@ -28,15 +72,16 @@ const Post = ({ id, username, userImg, img, caption }) => {
         alt={`${username}'s post`}
       />
       {/* Buttons */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4 ">
-          <HeartIcon className="postButton" />
-          <ChatIcon className="postButton" />
-          <PaperAirplaneIcon className="postButton rotate-45" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4 ">
+            <HeartIcon className="postButton" />
+            <ChatIcon className="postButton" />
+            <PaperAirplaneIcon className="postButton rotate-45" />
+          </div>
+          <BookmarkIcon className="postButton" />
         </div>
-        <BookmarkIcon className="postButton" />
-      </div>
-
+      )}
       {/* Caption */}
       <p className="p-5 truncate">
         <span className="font-bold mr-1">{username}</span>
@@ -44,17 +89,46 @@ const Post = ({ id, username, userImg, img, caption }) => {
       </p>
 
       {/* Comments */}
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+              <img
+                src={comment.data().userImage}
+                alt=""
+                className="h-7 rounded-full"
+              />
+              <p className="text-sm flex-1">
+                <span className="font-bold">{comment.data().username}</span>{" "}
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* Input Box */}
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          type="text"
-          placeholder="Add a comment"
-          className="border-none flex-1 focus:outline-none"
-        />
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+      {session && (
+        <form onSubmit={sendComment} className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comment"
+            className="border-none flex-1 focus:ring-0 outline-none"
+          />
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            className="font-semibold text-blue-400">
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 };
